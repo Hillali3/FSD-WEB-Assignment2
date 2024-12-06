a2af93cfdd1dd74cb85aba66a88faaa61e32a886
Hila <hila.ohana1910@gmail.com>
2024-12-06 21:48:39 +0200

    format routes

--- src/routes/user_route.ts
+++ src/routes/user_route.ts
@@ -1,25 +1,25 @@
-import { Router } from 'express';
+import { Router } from "express";
 
 const router = Router();
 const UserController = require("../controllers/user");
 
 router.post("/", (req, res) => {
-    UserController.createUser(req, res);
+  UserController.createUser(req, res);
 });
 router.get("/", (req, res) => {
-    UserController.getUsers(req, res);
+  UserController.getUsers(req, res);
 });
 router.get("/id/:id", (req, res) => {
-    UserController.getUserById(req, res);
+  UserController.getUserById(req, res);
 });
 router.get("/username/:username", (req, res) => {
-    UserController.getUserByUsername(req, res);
+  UserController.getUserByUsername(req, res);
 });
 router.put("/:id", (req, res) => {
-    UserController.updateUser(req, res);
+  UserController.updateUser(req, res);
 });
 router.delete("/:id", (req, res) => {
-    UserController.deleteUser(req, res);
+  UserController.deleteUser(req, res);
 });
 
 export default router;
