b61cd3661c484bf142349ba5b00d8e357260b166
Hila <hila.ohana1910@gmail.com>
2024-12-06 21:49:08 +0200

    format middleware

--- src/middlewares/authMiddleware.ts
+++ src/middlewares/authMiddleware.ts
@@ -1,11 +1,15 @@
-import { Request, Response, NextFunction } from 'express';
-import { verifyAccessToken } from '../utils/jwtUtils';
+import { Request, Response, NextFunction } from "express";
+import { verifyAccessToken } from "../utils/jwtUtils";
 
-export const authenticateJWT = (req: Request, res: Response, next: NextFunction) => {
-  const token = req.header('Authorization')?.replace('Bearer ', '');
+export const authenticateJWT = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const token = req.header("Authorization")?.replace("Bearer ", "");
 
   if (!token) {
-    return res.status(403).json({ message: 'Access denied' });
+    return res.status(403).json({ message: "Access denied" });
   }
 
   try {
@@ -13,6 +17,6 @@ export const authenticateJWT = (req: Request, res: Response, next: NextFunction)
     req.user = decoded;
     next();
   } catch (error) {
-    res.status(403).json({ message: 'Invalid or expired token' });
+    res.status(403).json({ message: "Invalid or expired token" });
   }
 };
