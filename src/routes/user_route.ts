cce3279d431b601623e6fc56a59cad2053295b88
Hila <hila.ohana1910@gmail.com>
2024-12-09 15:45:33 +0200

    add the auth middleware in user queries

--- src/routes/user_route.ts
+++ src/routes/user_route.ts
@@ -1,25 +1,26 @@
-import { Router } from 'express';
+import { Router } from "express";
 
 const router = Router();
 const UserController = require("../controllers/user");
+const authenticate = require("../middlewares/authMiddleware");
 
-router.post("/", (req, res) => {
-    UserController.createUser(req, res);
+router.post("/", authenticate, (req, res) => {
+  UserController.createUser(req, res);
 });
-router.get("/", (req, res) => {
-    UserController.getUsers(req, res);
+router.get("/", authenticate, (req, res) => {
+  UserController.getUsers(req, res);
 });
-router.get("/id/:id", (req, res) => {
-    UserController.getUserById(req, res);
+router.get("/id/:id", authenticate, (req, res) => {
+  UserController.getUserById(req, res);
 });
-router.get("/username/:username", (req, res) => {
-    UserController.getUserByUsername(req, res);
+router.get("/username/:username", authenticate, (req, res) => {
+  UserController.getUserByUsername(req, res);
 });
-router.put("/:id", (req, res) => {
-    UserController.updateUser(req, res);
+router.put("/:id", authenticate, (req, res) => {
+  UserController.updateUser(req, res);
 });
-router.delete("/:id", (req, res) => {
-    UserController.deleteUser(req, res);
+router.delete("/:id", authenticate, (req, res) => {
+  UserController.deleteUser(req, res);
 });
 
 export default router;
