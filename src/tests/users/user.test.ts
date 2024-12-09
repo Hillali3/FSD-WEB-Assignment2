589e9bf52aac6caeafeb746bf0540b454d28b604
Hila <hila.ohana1910@gmail.com>
2024-12-09 21:10:25 +0200

    fix create user test

--- src/tests/users/user.test.ts
+++ src/tests/users/user.test.ts
@@ -27,50 +27,51 @@ describe("Users Test", () => {
   test("Create User", async () => {
     const response = await request(app)
       .post("/users/")
-      .send({ user })
-      .set("Authorization", `Bearer ${accessToken}`);
+      .set("Content-Type", "application/json")
+      .set("Authorization", `Bearer ${accessToken}`)
+      .send(user);
     userId = response.body._id;
+    console.log("Response Body: ", response.body);
     expect(response.status).toBe(201);
     expect(response.body.username).toBe(user.username);
     expect(response.body.name).toBe(user.name);
-    expect(response.body.password).toBe(user.password);
     expect(response.body.email).toBe(user.email);
   });
-  // test("Get All Users", async () => {
-  //   const response = await request(app)
-  //     .get("/users/")
-  //     .set("Authorization", `Bearer ${accessToken}`);
-  //   expect(response.status).toBe(200);
-  //   expect(response.body.length).toBe(1);
-  // });
-  // test("Get User by ID", async () => {
-  //   const response = await request(app)
-  //     .get(`/users/id/${userId}`)
-  //     .set("Authorization", `Bearer ${accessToken}`);
-  //   expect(response.status).toBe(200);
-  //   expect(response.body.username).toBe(user.username);
-  //   expect(response.body.name).toBe(user.name);
-  //   expect(response.body.email).toBe(user.email);
-  // });
-  // test("Update User", async () => {
-  //   const response = await request(app)
-  //     .put(`/users/${userId}`)
-  //     .send({ name: "New Name" })
-  //     .set("Authorization", `Bearer ${accessToken}`);
-  //   expect(response.status).toBe(200);
-  //   expect(response.body.name).toBe("New Name");
-  // });
-  // test("Delete User", async () => {
-  //   const response = await request(app)
-  //     .delete(`/users/${userId}`)
-  //     .set("Authorization", `Bearer ${accessToken}`);
-  //   expect(response.status).toBe(200);
-  // });
-  // test("Get All Users after Delete", async () => {
-  //   const response = await request(app)
-  //     .get("/users/")
-  //     .set("Authorization", `Bearer ${accessToken}`);
-  //   expect(response.status).toBe(200);
-  //   expect(response.body.length).toBe(0);
-  // });
+  test("Get All Users", async () => {
+    const response = await request(app)
+      .get("/users/")
+      .set("Authorization", `Bearer ${accessToken}`);
+    expect(response.status).toBe(200);
+    expect(response.body.length).toBe(1);
+  });
+  test("Get User by ID", async () => {
+    const response = await request(app)
+      .get(`/users/id/${userId}`)
+      .set("Authorization", `Bearer ${accessToken}`);
+    expect(response.status).toBe(200);
+    expect(response.body.username).toBe(user.username);
+    expect(response.body.name).toBe(user.name);
+    expect(response.body.email).toBe(user.email);
+  });
+  test("Update User", async () => {
+    const response = await request(app)
+      .put(`/users/${userId}`)
+      .send({ name: "New Name" })
+      .set("Authorization", `Bearer ${accessToken}`);
+    expect(response.status).toBe(200);
+    expect(response.body.name).toBe("New Name");
+  });
+  test("Delete User", async () => {
+    const response = await request(app)
+      .delete(`/users/${userId}`)
+      .set("Authorization", `Bearer ${accessToken}`);
+    expect(response.status).toBe(200);
+  });
+  test("Get All Users after Delete", async () => {
+    const response = await request(app)
+      .get("/users/")
+      .set("Authorization", `Bearer ${accessToken}`);
+    expect(response.status).toBe(200);
+    expect(response.body.length).toBe(0);
+  });
 });
