0b45eb0d9d5442cb7cc8e8a660622958fac666db
Idan17 <idanbartov@gmail.com>
2024-12-09 18:52:26 +0200

    fix/swaggerConfig

--- src/swagger.ts
+++ src/swagger.ts
@@ -1,28 +1,39 @@
 import swaggerUi from 'swagger-ui-express';
 import { Express } from 'express';
 import swaggerJSDoc from 'swagger-jsdoc';
+import { Options } from 'swagger-jsdoc';
 
-// Swagger definition
-const swaggerDefinition = {
-  openapi: '3.0.0',
-  info: {
-    title: 'Node API with Swagger',
-    version: '1.0.0',
-    description: 'This is a simple API for user authentication and data handling.',
-  },
-  servers: [
-    {
-      url: 'http://localhost:3000', 
+
+// // Swagger definition
+// const swaggerDefinition = {
+//   openapi: '3.0.0',
+//   info: {
+//     title: 'Node API with Swagger',
+//     version: '1.0.0',
+//     description: 'This is a simple API for user authentication and data handling.',
+//   },
+//   servers: [
+//     {
+//       url: 'http://localhost:3000', 
+//     },
+//   ],
+// };
+
+// // Options for the swagger-jsdoc
+// const options = {
+//   swaggerDefinition,
+//   apis: ['./routes/*.ts'], 
+// };
+const options: Options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Your API Title',
+      version: '1.0.0',
     },
-  ],
+  },
+  apis: ['./src/routes/*.ts'], // Ensure this path matches your route files
 };
-
-// Options for the swagger-jsdoc
-const options = {
-  swaggerDefinition,
-  apis: ['./routes/*.ts'], 
-};
-
 const swaggerSpec = swaggerJSDoc(options);
 
 const setupSwaggerDocs = (app: Express) => {
